e30b924d9a4b8f8925348f40777fe7901809240a
Antagonisto <97778476+Antagonisto@users.noreply.github.com>
2023-06-06 21:31:52 +0300

    Add files via upload

---
+++ my.js
@@ -0,0 +1,12 @@
+
+
+$(document).ready(function() {
+  
+    $("#group-icon").tooltip({
+        content: "Поиск",
+        position: {
+            my: "left center",
+            at: "right top"
+        }
+    });
+})
